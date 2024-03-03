b6a8967373e2f76c0f1bbfb43ed29263d3ffd46b
arkaslittlemind <arkadiptamojumder14@gmail.com>
2024-03-04 01:13:14 +0530

    types

--- index.ts
+++ index.ts
@@ -1,2 +1,19 @@
-let age = 30
-console.log(age)
+let age: number = 30
+let firstName: string = 'John'
+let isFictional: boolean
+
+age = 31
+firstName = 'Jane'
+isFictional = true
+
+let planet = 'Earth'
+let moons = 1
+let isLarge = false
+
+// null and undefined
+
+let something: null
+let anotherThing: undefined
+
+something = null
+anotherThing = undefined
